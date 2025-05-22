ea31ea5c0854fff77a6739babba570b51114fde4
BaptisteDCL <baptiste.duclos2@gmail.com>
2025-05-22 18:52:29 +0200

    Ajout d'une route GET et POST pour ajouter une note dans un array du serveur

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,33 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+let notes = [];
+let nextId = 1;
+
 app.get("/", (req, res) => {
     res.json({ message : "Hello from backend 👋"});
+});
+
+app.get("/notes", (req, res) => {
+    res.json(notes);
+});
+
+app.post("/notes", (req, res) => {
+    const { title, content } = req.body;
+
+    if (!title || !content){
+        return res.status(400).json({ error : "Title and content are required."});
+    }
+
+    const newNote = {
+        id : nextId++,
+        title,
+        content,
+        createdAt: new Date().toISOString()
+    };
+
+    notes.push(newNote);
+    res.status(201).json(newNote);
 });
 
 app.listen(PORT, () => {

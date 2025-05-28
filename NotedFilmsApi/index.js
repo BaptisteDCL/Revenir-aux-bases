03ddbdd5ad7dd7435768f42bed630d7713b4a27e
BaptisteDCL <baptiste.duclos2@gmail.com>
2025-05-28 19:47:55 +0200

    Ajout d'une API de films et d'une route pour ajouter un film à une liste de films

---
+++ NotedFilmsApi/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+let films = [];
+let nextId = 1;
+
+app.get("/", (req, res) => {
+    res.json({ message : "Hello from backend 👋"});
+});
+
+app.get("/", (req, res)=>{
+    res.json(films);
+});
+
+app.post("/films", (req, res) =>{
+    const { title, content } = req.body;
+
+    if (!title || !content){
+        return res.status(404).json({ error : "Title and content are required."});
+    }
+
+    const newFilm = {
+        id : nextId++,
+        title,
+        content,
+        createdAt: new Date().toISOString()
+    };
+
+    films.push(newFilm);
+    res.status(201).json(newFilm);
+})
+
+app.listen(PORT, ()=>{
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+});

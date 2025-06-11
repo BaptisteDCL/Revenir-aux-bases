16a4895e626ce9aed2236e67b667a2bd6325eb2f
BaptisteDCL <baptiste.duclos2@gmail.com>
2025-06-11 19:50:57 +0200

    Changement de style

--- carWashAPI/voitures-app/src/App.tsx
+++ carWashAPI/voitures-app/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
+    <div className="w-full min-h-screen flex flex-col items-center justify-center bg-blue-100 text-gray-800">
       <h1>WashMyCar</h1>
       <p>Votre voiture, plus porpre que jamais</p>
       <button>Voir nos voitures</button>

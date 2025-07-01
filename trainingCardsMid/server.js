bc6edc110dc67de75ddacebf4977fea5c0416a59
BaptisteDCL <baptiste.duclos2@gmail.com>
2025-07-01 22:13:58 +0200

    Mise à jour de la fonction PUT de l'application pour ajouter des points

--- trainingCardsMid/server.js
+++ trainingCardsMid/server.js
@@ -28,8 +28,27 @@ app.get("api/cards", (req, res) =>{
         niveau = JSON.parse(data);
     });
 
+    cards = cards.filter(c => c.niveau === niveau);
     // On renvoie le json contenant toutes les informations cartes + niveau
     res.json({cards, niveau});
+})
+
+app.put("api/cards/:index/points", (req, res) => {
+    // On prends les données contenues dans les JSON
+    const cards = JSON.parse(fs.readFileSync(DATA_FILE, 'utf-8'));
+    const state = JSON.parse(fs.readFileSync(STATE_FILE, 'utf-8'));
+
+    // On récupère l'index envoyé par le coté client
+    const index = parseInt(req.params.index);
+    if (isNaN(index)) return res.status(400).json({message :  "L'index n'existe pas"});
+    const cardsDuNiveau = cards.filter(card => card.niveau == niveau);
+    const cardToUpdate = cardsDuNiveau[index];
+    if (!cardToUpdate) return res.status(404).json({ error: "Carte non trouvée" });
+    cardToUpdate.points += 1;
+    // Si le nombre de point dépasse le seuil, alors on passe au niveau Bronze
+    if (cardToUpdate.points >= 25) {
+        cardToUpdate.rang = "Bronze";
+    }
 })
 
 app.listen(PORT, () =>{
